5a81ee6c79ef6cc766fa6b78e522edc9da351138
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-04-26 16:32:20 +0530

    Update binary_search.cpp

--- sorting & searching/binary_search.cpp
+++ sorting & searching/binary_search.cpp
@@ -1,1 +1,50 @@
+// Binary Search in non-recusrive method
+// Time complexity :-
+// Best Case - O(1)
+// Average and worst case - O(logn)
+
+#include<stdio.h>
+int binarySearch(int arr[],int l,int r,int x)
+{
+	while(r>=l)
+	{
+		int mid=l+(r-l)/2;
+		if(arr[mid]==x)
+		{
+			return mid;
+		}
+		else if(arr[mid]<x)
+		{
+			l=mid+1;
+		}
+		else
+		{
+			r=mid-1;
+		}
+	}
+	return -1;
+}
+int main()
+{
+	int i,n,arr[50],x;
+	printf("\nEnter the size of an array:");
+	scanf("%d",&n);
+	printf("\nEnter the elements of an array:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("\nEnter the element to search:");
+	scanf("%d",&x);
+	int result=binarySearch(arr,0,n-1,x);
+	if(result==-1)
+	{
+		printf("\nElement is not present in the array.");
+	}
+	else
+	{
+		printf("\nElement is present at index %d",result);
+	}
+	return 0;
+}
 
