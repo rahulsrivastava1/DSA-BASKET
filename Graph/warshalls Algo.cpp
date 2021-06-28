a4a39c191832936ee5da1918d8dc9b13cb46d0a5
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-06-28 09:45:17 +0530

    Update warshalls Algo.cpp

--- Graph/warshalls Algo.cpp
+++ Graph/warshalls Algo.cpp
@@ -1,49 +1,55 @@
 // Warshall's algorithm
 
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-int max(int,int);
-void warshal(int p[10][10],int n) {
-	int i,j,k;
-	for (k=1;k<=n;k++)
-	  for (i=1;i<=n;i++)
-	   for (j=1;j<=n;j++)
-	    p[i][j]=max(p[i][j],p[i][k]&&p[k][j]);
+include <bits/stdc++.h>
+using namespace std;
+#define V 4
+#define INF 99999
+void printSolution(int dist[][V]);
+void floydWarshall(int graph[][V])
+{
+	int dist[V][V],i,j,k;
+	for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+    for (k = 0; k < V; k++)
+	{
+		for (i = 0; i < V; i++)
+		{
+			for (j = 0; j < V; j++)
+			{
+				if (dist[i][j] > (dist[i][k] + dist[k][j])
+                    && (dist[k][j] != INF
+                        && dist[i][k] != INF))
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+    	}
+	}
+	 printSolution(dist);
 }
-int max(int a,int b) {
-	;
-	if(a>b)
-	 return(a); else
-	 return(b);
+void printSolution(int dist[][V])
+{
+    cout << "The following matrix shows the shortest "
+            "distances"
+            " between every pair of vertices \n";
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF"
+                     << "     ";
+            else
+                cout << dist[i][j] << "     ";
+        }
+        cout << endl;
+    }
 }
-int main() {
-	int p[10][10]= {
-		0
-	}
-	,n,e,u,v,i,j;
-	clrscr();
-	printf("\n Enter the number of vertices:");
-	scanf("%d",&n);
-	printf("\n Enter the number of edges:");
-	scanf("%d",&e);
-	for (i=1;i<=e;i++) {
-		printf("\n Enter the end vertices of edge %d:",i);
-		scanf("%d%d",&u,&v);
-		p[u][v]=1;
-	}
-	printf("\n Matrix of input data: \n");
-	for (i=1;i<=n;i++) {
-		for (j=1;j<=n;j++)
-		   printf("%d\t",p[i][j]);
-		printf("\n");
-	}
-	warshal(p,n);
-	printf("\n Transitive closure: \n");
-	for (i=1;i<=n;i++) {
-		for (j=1;j<=n;j++)
-		   printf("%d\t",p[i][j]);
-		printf("\n");
-	}
-	return 0;
+
+// driver code
+int main()
+{
+	int graph[V][V] = { { 0, 5, INF, 10 },
+                        { INF, 0, 3, INF },
+                        { INF, INF, 0, 1 },
+                        { INF, INF, INF, 0 } };
+    floydWarshall(graph);
+    return 0;
 }
