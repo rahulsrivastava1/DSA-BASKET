4c1d1f3a7b8c2111f2f2ae9e7fddc0d1a84bd6cc
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-10-29 01:13:43 +0530

    Create levelOrderTraversal.cpp

---
+++ Tree/levelOrderTraversal.cpp
@@ -0,0 +1,42 @@
+// only function
+// The function will return vector of vectors containing level order traversal nodes
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+vector<vector<int>> levelOrder(TreeNode* root)
+{
+  vector<vector<int>> ans;
+  if(root==NULL)
+    return ans;
+  queue<TreeNode*> q;
+  q.push(root);
+  while(!q.empty())
+  {
+    int size=q.size();
+    vector<int> level;
+    for(int j=0;j<size;j++)
+    {
+      TreeNode* temp=q.front();
+      q.pop();
+      if(temp->left)
+        q.push(temp->left);
+      if(temp->right)
+        q.push(temp->right);
+      level.push(temp->val);
+    }
+    ans.push(level);
+  }
+  return ans;
+}
+    
+  
