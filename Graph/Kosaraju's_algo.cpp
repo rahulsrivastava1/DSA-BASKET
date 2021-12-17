4834ac04e5ae18a04e192fac60977403d061a8f0
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-12-17 11:46:24 +0530

    Create Kosaraju's_algo.cpp

---
+++ Graph/Kosaraju's_algo.cpp
@@ -0,0 +1,65 @@
+// Kosaraju's Algorithm for Strongly Connected Graph
+// Time Complexity :- O(N+E)
+// Space Complexity :- O(N+E)+O(N)+O(N)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void dfs(int node,stack<int>& st,vector<int>& vis,vector<int> adj[]){
+  vis[node]=1;
+  for(auto it:adj[node]){
+    if(!vis[it])
+      dfs(it,st,vis,adj);
+  }
+  st.push(node);
+}
+
+void reverse_dfs(int node,vector<int>& vis,vector<int> transpose[]){
+  cout<<node<<" ";
+  vis[node]=1;
+  for(auto it:adj[node]){
+    if(!vis[it]
+       reverse_dfs(it,vis,transpose);
+  }
+}
+
+// driver code
+int main(){
+  int tc;
+  cin>>tc;
+  while(tc--){
+    int V,E;
+    cin>>V>>E;
+    vector<int> adj[V];
+    for(int i=0;i<E;i++){
+      int u,v;
+      cin>>u>>v;
+      adj[u].push_back(v);
+    }
+    stack<int> st;
+    vector<int> vis(V,0);
+    for(int i=0;i<V;i++){
+      if(!vis[i])
+        dfs(i,st,vis,adj);
+    }
+    
+    // transpose of the graph
+    vector<int> transpose(V);
+    for(int i=0;i<V;i++){
+      vis[i]=0;
+      for(auto it:adj[i])
+        transpose[it].push_back(i);
+    }
+    while(!st.empty()){
+      int node=st.top();
+      st.pop();
+      if(!vis[node]){
+        cout<<"SCC :";
+        reverse_dfs(node,vis,transpose);
+        cout<<endl;
+      }
+    }
+    
+  }
+  return 0;
+}
