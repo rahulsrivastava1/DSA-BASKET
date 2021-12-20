2afeefaeb5028427f7e652a2b9e0f3b046ceface
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-12-20 21:35:27 +0530

    Create k-th_symbol_in_grammar.cpp

---
+++ Recursion/k-th_symbol_in_grammar.cpp
@@ -0,0 +1,13 @@
+// k-th Symbol in Grammar
+
+// function
+int kthGrammar(int n, int k) {
+        if(n==1 && k==1)
+            return 0;
+        int mid=pow(2,n-1)/2;
+        if(k<=mid)
+            return kthGrammar(n-1,k);
+        else
+            return !kthGrammar(n-1,k-mid);
+        return 0;
+    }
