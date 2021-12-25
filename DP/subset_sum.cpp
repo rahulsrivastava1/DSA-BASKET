8f3a7c391f2e2fb20d18f60d371a4f8b4effa001
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-12-25 17:02:23 +0530

    Update subset_sum.cpp

--- DP/subset_sum.cpp
+++ DP/subset_sum.cpp
@@ -14,7 +14,7 @@ bool isSubsetSum(int N,int arr[],int sum){
   }
   for(int i=1;i<N+1;i++){
     for(int j=1;j<sum+1;j++){
-      if(arr[i-1]<=sum)
+      if(arr[i-1]<=j)
         dp[i][j]=dp[i-1][j-arr[i-1] || dp[i-1][j];
       else
         dp[i][j]=dp[i-1][j];
