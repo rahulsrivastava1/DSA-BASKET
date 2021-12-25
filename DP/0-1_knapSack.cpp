3c5c1d103e2e860ae5366034eb9040dca1e1e4e8
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-12-25 12:39:47 +0530

    Update 0-1_knapSack.cpp

--- DP/0-1_knapSack.cpp
+++ DP/0-1_knapSack.cpp
@@ -13,7 +13,8 @@ int knapSack(int W, int wt[], int val[], int n)
         }
     }
 
-// DP- approach
+// DP - approach
+// Recursion + Memoization
 
 class Solution
 {
@@ -36,3 +37,27 @@ class Solution
         }
     }
 };
+
+// DP - approach
+// Top Down Approach
+
+// function
+int knapSack(int W,int wt[],int val[],int n){
+    int dp[n+1][W+1];
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<W+1;j++){
+            if(i==0 || j==0)
+                dp[i][j]=0;
+        }
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<W+1;j++){
+            if(wt[i-1]<=j){
+                dp[i][j]=max(val[i-1]+dp[n-1][j-wt[i-1],dp[i-1][j]);
+            }else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+   return dp[n][W];
+}
