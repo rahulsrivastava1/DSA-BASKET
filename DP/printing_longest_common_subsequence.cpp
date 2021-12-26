cff85042e75d5280aa8c515ffef7cbf6a6511a1b
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-12-26 17:18:25 +0530

    Create printing_longest_common_subsequence.cpp

---
+++ DP/printing_longest_common_subsequence.cpp
@@ -0,0 +1,36 @@
+// Printing longest common Subsequence
+
+string lcs(int x, int y, string s1, string s2)
+    {
+        int dp[x+1][y+1];
+        for(int i=0;i<x+1;i++){
+            for(int j=0;j<y+1;j++){
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<x+1;i++){
+            for(int j=1;j<y+1;j++){
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        string str;
+        int i=x,j=y;
+        while(i>0 && j>0){
+          if(s1[i-1]==s2[j-1]){
+            str.push_back(s1[i-1]);
+            i--;
+            j--;
+          }else{
+            if(dp[i][j-1]>dp[i-1][j])
+              j--;
+            else
+              i--;
+          }
+        }
+        reverse(str.begin(),str.end());
+        return str;
+    }
