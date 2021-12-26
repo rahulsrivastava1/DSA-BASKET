4b5a5ecef6dfa4609c36f866f1e4b0726d9ae7ad
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-12-26 18:19:42 +0530

    Create longest_palindromic_subsequence.cpp

---
+++ DP/longest_palindromic_subsequence.cpp
@@ -0,0 +1,29 @@
+// Longest Palindromic Subsequence
+
+// utility function
+int lcs(int x, int y, string s1, string s2)
+    {
+        int dp[x+1][y+1];
+        for(int i=0;i<x+1;i++){
+            for(int j=0;j<y+1;j++){
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<x+1;i++){
+            for(int j=1;j<y+1;j++){
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return dp[x][y];
+    }
+
+// main function
+int LPS(string a){
+  string b=reverse(a.begin(),a.end());
+  int x=a.size();
+  return helperLCS(x,x,a,b);
+}
