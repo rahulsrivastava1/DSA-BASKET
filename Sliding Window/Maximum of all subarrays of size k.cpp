6bd21e324e748ae33edc99458de488228d8a35a8
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2022-01-29 20:55:25 +0530

    Update Maximum of all subarrays of size k.cpp

--- Sliding Window/Maximum of all subarrays of size k.cpp
+++ Sliding Window/Maximum of all subarrays of size k.cpp
@@ -1,16 +1,25 @@
 //Maximum of all subarrays of size k
 
 // function
-vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
-        vector<int> ret;
-       deque<int> dq;
-       for(int i=0;i<n;i++){
-           while(!dq.empty() and arr[i]>=arr[dq.back()]) dq.pop_back();
-           dq.push_back(i);
-           if(i>=k-1){
-               if(i-k==dq.front()) dq.pop_front();
-               ret.push_back(arr[dq.front()]);
-           }
-       }
-       return ret;
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int i=0;
+        int j=0;
+        vector<int> ans;
+        list<int> l;
+        int n=nums.size();
+        while(j<n){
+            while(l.size()>0 && l.back()<nums[j])
+                l.pop_back();
+            l.push_back(nums[j]);
+        if(j-i+1<k)
+            j++;
+        else if(j-i+1==k){
+            ans.push_back(l.front());
+            if(l.front()==nums[i])
+                l.pop_front();
+            i++;
+            j++;
+        }
+        }
+        return ans;  
     }
