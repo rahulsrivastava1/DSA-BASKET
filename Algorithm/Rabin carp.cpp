27a43a750637d9218d6037b835d2d282b144fbd9
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2022-06-06 01:01:21 +0530

    Create Rabin carp.cpp

---
+++ Algorithm/Rabin carp.cpp
@@ -0,0 +1,40 @@
+// Rabin carp algorithm
+
+vector<int> stringMatch(string &str, string &pat) {
+    int n=str.size();
+    int m=pat.size();
+    vector<int> ans;
+    int hashP=0;
+    int hashT=0;
+    for(int i=0;i<m;i++){
+        hashP+=((pat[i]-'A'+1)*pow(5,i));
+        hashT+=((str[i]-'A'+1)*pow(5,i));
+    }
+    if(hashP==hashT){
+        bool isEqual=true;
+        for(int i=0;i<m;i++){
+            if(pat[i]!=str[i]){
+                isEqual=false;
+                break;
+            }
+        }
+        if(isEqual)
+            ans.push_back(0);
+    }
+    int flag=0;
+    for(int i=m;i<n;i++){
+        hashT=((hashT-(str[i-m]-'A'+1))/5)+((str[i]-'A'+1)*pow(5,m-1));
+        if(hashT==hashP){
+            bool isEqual=true;
+            for(int j=0;j<m;j++){
+                if(pat[j]!=str[i-m+1+j]){
+                    isEqual=false;
+                    break;
+                }
+            }
+            if(isEqual)
+                ans.push_back(i-m+1);
+        }
+    }
+    return ans;
+}
