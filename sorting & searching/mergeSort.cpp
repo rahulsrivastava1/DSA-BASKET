027c0cf620f9e6c03f07c31979e022de6629a396
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2022-07-14 00:01:55 +0530

    Update mergeSort.cpp

--- sorting & searching/mergeSort.cpp
+++ sorting & searching/mergeSort.cpp
@@ -1,43 +1,40 @@
 // Merge Sort
 // Time Complexity :- O(nlogn)
 
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void merge(int arr[],int l,int mid,int r){
-	int i=l,j=mid+1,k=0,temp[r-l+1];
-	while(i<=mid && j<=r){
-		if(arr[i]<=arr[j])
-			temp[k++]=arr[i++];
-		else
-			temp[k++]=arr[j++];
-	}
-	while(i<=mid)
-		temp[k++]=arr[i++];
-	while(j<=r)
-		temp[j++]=arr[j++];
-	int size=sizeof(temp)/sizeof(temp[0]);
-	for(i=0,j=l;i<size;i++,j++)
-		arr[j]=temp[i];
+void merge(int arr[],int low,int mid,int high){
+    int i=low,j=mid+1,k=0,temp[high-low+1];
+    while(i<=mid && j<=high){
+        if(arr[i]<=arr[j])
+            temp[k++]=arr[i++];
+        else
+            temp[k++]=arr[j++];
+    }
+    while(i<=mid)
+        temp[k++]=arr[i++];
+    while(j<=high)
+        temp[k++]=arr[j++];
+    int x=sizeof(temp)/sizeof(temp[0]);
+    for(int i=0,j=low;i<x;i++,j++)
+        arr[j]=temp[i];
 }
 
-void mergeSort(int arr[],int l,int r){
-	if(l<r){
-		int mid=l+(r-l)/2;
-		mergeSort(arr,l,mid);
-		mergeSort(arr,mid+1,r);
-		merge(arr,l,mid,r);
-	}
+void mergeSort(int arr[],int low,int high){
+    if(low<high){
+        int mid=low+(high-low)/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
 }
 
-int main(){
-	int n;
-	cin>>n;
-	int arr[n];
-	for(int i=0;i<n;i++)
-		cin>>arr[i];
-	mergeSort(arr,0,n-1);
-	for(int i=0;i<n;i++)
-		cout<<arr[i]<<" ";
+int main() {
+	int arr[]={12,11,13,5,6,7};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    mergeSort(arr,0,n-1);
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
 	return 0;
 }
