677cc303fa35d2e1682ffa75de259aa9ddeb78af
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2022-08-16 17:17:14 +0530

    Create Multiply by 2 and divide by 2.cpp

---
+++ Number System/Multiply by 2 and divide by 2.cpp
@@ -0,0 +1,12 @@
+// Multiply by 2 and divide by 2
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n=25;
+  // multiply
+  cout<<"After Multiplied by 2:"<<(n<<1)<<endl;
+  cout<<"After Divided by 2:"<<(n>>1)<<endl;
+  return 0;
+}
