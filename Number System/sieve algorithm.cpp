1f62ed3b7afb953ef25eecd737c497a6e256a16d
Rahul Srivastava <srivastavar433@gmail.com>
2022-09-11 16:34:37 +0530

    Create sieve algorithm.cpp

---
+++ Number System/sieve algorithm.cpp
@@ -0,0 +1,27 @@
+// Prime Factorisation
+// Sieve Algorithm
+
+// Time Complexity :- O(nlog(log(n)))
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=1e7+10;
+vector<bool> isPrime(n,1);
+
+void sieve(int n){
+    isPrime[0]=isPrime[1]=0;
+    for(int i=2;i<n;i++){
+        if(isPrime[1]){
+            for(int j=2*i;j<n;j+=i)
+                isPrime[j]=0;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    sieve(n);
+	  return 0;
+}
